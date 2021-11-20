55c57fa7dab973654d2c54b36545f8643d9fd62d
kizzy <kizzy@gmail.com>
2021-11-20 05:39:01 -0500

    file created

---
+++ src/test/java/com/Devops/devop/DevopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Devops.devop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
